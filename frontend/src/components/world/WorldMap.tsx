2382f34be8c7008dcc644f49466900c3f86c4586
Ziqian Ge <ge.zi@northeastern.edu>
2021-04-02 21:21:30 +0800

    fix too many calls to 'emitMovement' (#16)

--- frontend/src/components/world/WorldMap.tsx
+++ frontend/src/components/world/WorldMap.tsx
@@ -181,7 +181,11 @@ class CoveyGameScene extends Phaser.Scene {
       const isMoving = primaryDirection !== undefined;
       this.player.label.setX(body.x);
       this.player.label.setY(body.y - 20);
-      if (!this.lastLocation || this.lastLocation.x !== body.x || this.lastLocation.y !== body.y || this.lastLocation.rotation !== primaryDirection || this.lastLocation.moving !== isMoving) {
+      if (!this.lastLocation
+        || this.lastLocation.x !== body.x
+        || this.lastLocation.y !== body.y
+        || (isMoving && this.lastLocation.rotation !== primaryDirection)
+        || this.lastLocation.moving !== isMoving) {
         if (!this.lastLocation) {
           this.lastLocation = {
             x: body.x,
